96c3113de79f45686ab49de19a81bebd14bc4cee
Saurabh Shukla <saurabh@yapapaya.com>
2017-01-01 22:45:51 +0530

    Fix log levels

--- index.js
+++ index.js
@@ -11,15 +11,27 @@ var program = require('commander');
 program
 	.version( '0.0.1' )
   .description('BootStrap a WordPress plugin theme or plugin')
-	.option( '--verbose', 'Detailed log output' )
+	.option( '--verbose', 'Verbose log output' )
+	.option( '--debug', 'Detailed debug output in log' )
+	.option( '--silly', 'Extra detailed debug output in log' )
 	.option( '--no-autofix', 'Fix errors in package.json & update it')
 	.option( '--no-validate', 'Don\'t validate git repositories' )
 	.option( '--no-css', 'Don\'t generate style.css from style.scss' )
 	.option( '--no-push', 'Don\'t commit and push after bootstrapping' )
 	.parse( process.argv );
 
-var verbose = ('verbose' in program )? true: false;
+var logLevel = 'info';
 
-var wpgg = require("./lib/wpgg.js").init(program.validate, program.autofix, program.css, program.push, verbose);
+var logLevel = ('verbose' in program )? 'verbose': logLevel;
+
+if(logLevel === ''){
+	logLevel = ('debug' in program )? 'debug': logLevel;
+}
+
+if(logLevel === ''){
+	logLevel = ('silly' in program )? 'silly': logLevel;
+}
+
+var wpgg = require("./lib/wpgg.js").init(program.validate, program.autofix, program.css, program.push, logLevel);
 
 wpgg.initConfig();

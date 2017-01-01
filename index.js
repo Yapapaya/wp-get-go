52f531ffe8ab3189f8b8a240f71ed2cfd2d01c3e
Saurabh Shukla <saurabh@yapapaya.com>
2017-01-01 22:30:09 +0530

    Refactoring to node only

--- index.js
+++ index.js
@@ -2,41 +2,24 @@
 
 'use strict';
 
-/*
- * 
- * Why not call this wp-crank or wp-get-go(ing) because it cranks out WordPress projects
- */
 
 var program = require('commander');
 
-var log = require("winston"); // for logging
-
 /*
- * cmd `wpbootstrap --no-validate --no-css --no-push`
+ * cmd `wpgg --verbose --no-validate --no-css --no-push`
 */
-
 program
 	.version( '0.0.1' )
-.command('wp-gg')
-.alias('wpgetgo')
-  .alias('wpgg')
-  .alias('wp-get-go')
-  .alias('wp-get-go')
   .description('BootStrap a WordPress plugin theme or plugin')
-	// .option( '--log [level]', 'The level of logging desired' )
+	.option( '--verbose', 'Detailed log output' )
+	.option( '--no-autofix', 'Fix errors in package.json & update it')
 	.option( '--no-validate', 'Don\'t validate git repositories' )
 	.option( '--no-css', 'Don\'t generate style.css from style.scss' )
 	.option( '--no-push', 'Don\'t commit and push after bootstrapping' )
-	.option('-v, --verbose', 'Detailed log output')
 	.parse( process.argv );
 
+var verbose = ('verbose' in program )? true: false;
 
-console.log('Starting bootstrap process...');
+var wpgg = require("./lib/wpgg.js").init(program.validate, program.autofix, program.css, program.push, verbose);
 
-var wpbs = require("./lib/wpbs.js");
-
-wpbs.init(program.validate, program.css, program.push, program.verbose);
-
-console.log('Parsing Package Information...');
-
-wpbs.initConfig();
+wpgg.initConfig();

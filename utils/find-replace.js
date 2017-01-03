73372fb237e977ced52f45e68f75cb1ecfaf9082
Saurabh Shukla <saurabh@yapapaya.com>
2017-01-03 09:48:07 +0530

    Refactor replace and utilities

---
+++ utils/find-replace.js
@@ -0,0 +1,91 @@
+/**
+ * Starter Package Fetch Module
+ * 
+ * @module utils/find-replace
+ * @todo remove the footer.php special condition & release this as a generic package
+ */
+var glob = require( 'glob' ); // for finding files using glob patterns
+var readline = require( 'readline' ); // for reading files line by line
+var fs = require( 'fs' ); // for file system operations
+
+var findReplace = module.exports = function( options, callback ) {
+
+	if ( !'silent' in options ) {
+		options.silent = true;
+	}
+	
+	if ( !'path' in options ) {
+		options.path = './';
+	}
+
+	var patterns;
+
+	if ( typeof options.files === 'string' ) {
+		patterns = [ options.files ];
+	} else {
+		patterns = options.files;
+	}
+
+	var path = options.path;
+
+	patterns.forEach( function( pattern ) {
+
+		find( path, pattern, options, callback );
+
+	} );
+
+};
+
+var find = function( path, pattern, options, callback ) {
+	var findablePattern = path + pattern;
+	glob( findablePattern, { nodir: true }, function( err, files ) {
+
+		if ( err ) {
+			throw err;
+		}
+
+		files.forEach( function( file ) {
+
+			if ( options.silent === false ) {
+				console.log( 'Found file "' + file + '" from pattern "' + pattern + '"' );
+			}
+
+			if ( file.search( '/footer.php' ) > -1 ) {
+				options.match = options.match.concat( options.Colophon );
+			}
+
+			replaceFile( file, options.match, callback );
+
+		} );
+
+	} );
+};
+
+var replace = function( file, options, callback ) {
+
+	var output = '';
+
+	var match = options.match;
+
+	var rl = readline.createInterface( {
+		input: fs.createReadStream( file )
+	} );
+
+	rl.on( 'line', function( line ) {
+		var replaced = line;
+		Object.keys( match ).forEach( function( key ) {
+			replaced = replaced.replace( match[key].replace, match[key].with );
+		} );
+		if ( options.silent === false ) {
+			console.log( 'Replaced "' + file + '"' );
+		}
+		output += replaced + '\r\n';
+	} );
+
+	rl.on( 'close', function() {
+		fs.writeFile( file, output, 'utf8', function( err ) {
+			callback( err, file, output );
+		} );
+	} );
+
+};
---
+++ utils/utils.js
@@ -0,0 +1,40 @@
+/**
+ * General utility functions
+ * 
+ * @module utils/utils.js
+ */
+var fs = require( 'fs' ); // for file system operations
+
+var utils = function(){
+	
+};
+
+/**
+ * Return an equivalent svn url for a given git url on GitHub
+ * 
+ * @param {string} gitURL git URL of repo
+ * @returns {string} https url
+ */
+utils.svnURL = function( gitURL ) {
+	return gitURL.replace( /:/gi, "/" ).replace( /^git@/gi, "https://" ).replace( /.git$/gi, "/" );
+};
+
+/**
+ * Read contents of a json file into a json object
+ * 
+ * @param {type} path
+ * @returns {JSON.parse.j|Array|Object}
+ */
+utils.readJSON = function( path ) {
+	if ( !fs.existsSync( path ) ) {
+		throw new Error( 'File ' + path + ' not found' );
+	}
+	var obj = JSON.parse( fs.readFileSync( path, 'utf8' ) );
+
+	return obj;
+
+};
+
+module.exports = utils;
+
+

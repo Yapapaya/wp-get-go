dd523abdb091e670fc25715529eae7eb3fcdb248
Saurabh Shukla <saurabh@yapapaya.com>
2017-01-06 09:07:35 +0530

    Making room for componets

--- utils/utils.js
+++ utils/utils.js
@@ -5,8 +5,8 @@
  */
 var fs = require( 'fs' ); // for file system operations
 
-var utils = function(){
-	
+var utils = function() {
+
 };
 
 /**
@@ -33,6 +33,39 @@ utils.readJSON = function( path ) {
 
 	return obj;
 
+};
+
+utils.mkdir_p = function(path, mode, callback, position) {
+    mode = mode || 0777;
+    position = position || 0;
+    var parts = require('path').normalize(path).split('/');
+
+    if (position >= parts.length) {
+        if (callback) {
+            return callback();
+        } else {
+            return true;
+        }
+    }
+
+    var directory = parts.slice(0, position + 1).join('/');
+    fs.stat(directory, function(err) {
+        if (err === null) {
+            mkdir_p(path, mode, callback, position + 1);
+        } else {
+            fs.mkdir(directory, mode, function (err) {
+                if (err) {
+                    if (callback) {
+                        return callback(err);
+                    } else {
+                        throw err;
+                    }
+                } else {
+                    utils.mkdir_p(path, mode, callback, position + 1);
+                }
+            });
+        }
+    });
 };
 
 module.exports = utils;
